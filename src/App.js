4e0892bc49d8ca5b65d5730496264235c65b622d
lisicky <lisicky@inbox.ru>
2023-02-01 16:33:36 +0400

    example of config for CSL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {BigNum} from "@emurgo/cardano-serialization-lib-browser";
 
 function App() {
+  const numm = BigNum.from_str("33333");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload {numm.to_hex()} {numm.to_json()}.
         </p>
         <a
           className="App-link"
